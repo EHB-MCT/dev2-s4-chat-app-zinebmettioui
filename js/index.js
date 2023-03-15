3582486d583711c97ad6491c61cba460217e5805
zinebmettioui <114058287+zinebmettioui@users.noreply.github.com>
2023-03-15 14:26:47 +0100

    commit1

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
 "use strict";
 
 const chat = {
-    author: "yourName",
-    init() {
-    },
-    sendMessage() {
-    },
-    fetchMessages() {
-    },
-    renderMessage(message) {
-    }
+	author: "yourName",
+	init() {
+		this.fetchMessages();
+	},
 
-}
+	sendMessage() {},
+	fetchMessages() {
+		// TODO's get message
+		fetch("https://dev2chat.onrender.com/messages")
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (messages) {
+				console.log(messages);
+				// TODO's show messages (using renderMessages)
+				document.querySelector("messageContainer").innerHTML = messages[0].messages;
+			});
+	},
+
+	renderMessage(message) {},
+};
+// TODO's messages --> HTML
 
 chat.init();
